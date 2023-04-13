09d900d6261f191105eed32cbc937435c7c161a4
JasperGrant <72110751+JasperGrant@users.noreply.github.com>
2023-04-12 22:24:33 -0300

    Added day one additions

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+//Declare memory of emulator
+char memory[4096];
+
+//Declare registers of emulator
+//TODO: Add Macros for shorthand
+char registers[0xF];
+
 int main() {
     printf("Hello, World!\n");
     return 0;
+
+    //Fetch
+
+    //Decode
+
+    //Execute
 }
